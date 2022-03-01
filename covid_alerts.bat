3039874bb8040e18b4d50ae604346ea6e138665f
Nigel-Mark-Smith <58214346+Nigel-Mark-Smith@users.noreply.github.com>
2022-03-01 10:41:28 +0000

    Changes to alert parameters
    Parameters changed following decline in Omicron cases.

--- covid_alerts.bat
+++ covid_alerts.bat
@@ -2,4 +2,4 @@
 rem This batch file executes script general_alerts.py 
 rem which will inform the user of any concerns relating 
 rem to the latest available COVID-19 data.
-general_alerts.py
+general_alerts.py | findstr WARNING
